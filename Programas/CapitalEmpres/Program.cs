80bd7d4fec0401f430e04b3140733681741ff313
JADPA-08 <JADPA-08@JADPA-05>
2022-08-04 10:59:18 -0600

    Agregar archivos de proyecto.

---
+++ Programas/CapitalEmpres/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Capital de la empresa");
+Console.WriteLine(" ");
+
+double s1,s2, s3;
+
+Console.WriteLine("Ingrese la aportacion del socio 1");
+s1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la aportacion del socio 2");
+s2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la aportacion del socio 3");
+s3 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("__________________________");
+Console.WriteLine("el capital es:" +);
+
+
+
+
---
+++ Programas/ControlVentaProducto/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("control de venta de producto");
+Console.WriteLine(" ");
+
+double p,cantidad;
+
+Console.WriteLine("Ingrese la cantidad del producto");
+cantidad = int.Parse(Console.ReadLine());
+
+double ic = cantidad * p;
+double id = 11/100 * ic;
+double ip = ic - id;
+
+Console.WriteLine("__________________________");
+Console.WriteLine("Compra:   " +ic.ToString("0.00"));
+Console.WriteLine("Descuento:" +id.ToString("0.00"));
+Console.WriteLine("Pago:     " +ip.ToString("0.00"));
+
+
+
